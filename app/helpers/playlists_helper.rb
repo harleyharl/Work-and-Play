d50a5d9806138e65de325e2231cb00566f04429b
harleyharl <harleymteden@gmail.com>
2019-03-23 11:14:18 -0700

    refactoring locations edit view

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -5,33 +5,33 @@ module PlaylistsHelper
   #   location.playlists.count
   # end
   #
-  def daily_playlist_schedule(location)
-    # binding.pry
-    # if location.playlists.count == 0
-    #   link_to(location.playlists[0].name, location_playlist_path(location, location.playlists[0]))
-    if location.playlists.count == 1
-      link_to(location.playlists[0].name, location_playlist_path(location, location.playlists[0]))
-    elsif location.playlists.count == 2
-      link_to(location.playlists[0].name, location_playlist_path(location, location.playlists[0]))
-      link_to(location.playlists[1].name, location_playlist_path(location, location.playlists[1]))
-    elsif location.playlists.count == 3
-      link_to(location.playlists[0].name, location_playlist_path(location, location.playlists[0]))
-      link_to(location.playlists[1].name, location_playlist_path(location, location.playlists[1]))
-      link_to(location.playlists[2].name, location_playlist_path(location, location.playlists[2]))
-    elsif location.playlists.count == 4
-      link_to(location.playlists[0].name, location_playlist_path(location, location.playlists[0]))
-      link_to(location.playlists[1].name, location_playlist_path(location, location.playlists[1]))
-      link_to(location.playlists[2].name, location_playlist_path(location, location.playlists[2]))
-      link_to(location.playlists[3].name, location_playlist_path(location, location.playlists[3]))
-    elsif location.playlists.count == 5
-      # binding.pry
-      link_to(location.playlists[0].name, location_playlist_path(location, location.playlists[0])) +
-      link_to(location.playlists[1].name, location_playlist_path(location, location.playlists[1])) +
-      link_to(location.playlists[2].name, location_playlist_path(location, location.playlists[2])) +
-      link_to(location.playlists[3].name, location_playlist_path(location, location.playlists[3])) +
-      link_to(location.playlists[4].name, location_playlist_path(location, location.playlists[4]))
-    end
-  end
+  # def daily_playlist_schedule(location)
+  #   # binding.pry
+  #   # if location.playlists.count == 0
+  #   #   link_to(location.playlists[0].name, location_playlist_path(location, location.playlists[0]))
+  #   if location.playlists.count == 1
+  #     link_to(location.playlists[0].name, location_playlist_path(location, location.playlists[0]))
+  #   elsif location.playlists.count == 2
+  #     link_to(location.playlists[0].name, location_playlist_path(location, location.playlists[0]))
+  #     link_to(location.playlists[1].name, location_playlist_path(location, location.playlists[1]))
+  #   elsif location.playlists.count == 3
+  #     link_to(location.playlists[0].name, location_playlist_path(location, location.playlists[0]))
+  #     link_to(location.playlists[1].name, location_playlist_path(location, location.playlists[1]))
+  #     link_to(location.playlists[2].name, location_playlist_path(location, location.playlists[2]))
+  #   elsif location.playlists.count == 4
+  #     link_to(location.playlists[0].name, location_playlist_path(location, location.playlists[0]))
+  #     link_to(location.playlists[1].name, location_playlist_path(location, location.playlists[1]))
+  #     link_to(location.playlists[2].name, location_playlist_path(location, location.playlists[2]))
+  #     link_to(location.playlists[3].name, location_playlist_path(location, location.playlists[3]))
+  #   elsif location.playlists.count == 5
+  #     # binding.pry
+  #     link_to(location.playlists[0].name, location_playlist_path(location, location.playlists[0])) +
+  #     link_to(location.playlists[1].name, location_playlist_path(location, location.playlists[1])) +
+  #     link_to(location.playlists[2].name, location_playlist_path(location, location.playlists[2])) +
+  #     link_to(location.playlists[3].name, location_playlist_path(location, location.playlists[3])) +
+  #     link_to(location.playlists[4].name, location_playlist_path(location, location.playlists[4]))
+  #   end
+  # end
 
   # def playlist_schedule(location)
   #   location.playlists.each do |playlist|
@@ -41,10 +41,13 @@ module PlaylistsHelper
   #   end.join("\n").html_safe
   # end
 
+  def playlist_times
+    ["6am-9am", "9am-12pm", "12pm-3pm", "3pm-6pm", "6pm-9pm"]
+  end
+
   def playlist_schedule(location)
-    # binding.pry
-    location.playlists.each do |playlist|
-      concat(link_to(playlist.name, location_playlist_path(location, playlist))).concat(content_tag(:br))
+    location.playlists.each_with_index do |playlist, index|
+      concat(content_tag :h3, "#{playlist_times[index]}").concat(link_to(playlist.name, location_playlist_path(location, playlist))).concat(content_tag(:br))
     end.join("\n").html_safe
   end
 
