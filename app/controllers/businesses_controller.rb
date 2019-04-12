a137e9d7b353159301a140aec6976507640cee00
harleyharl <harleymteden@gmail.com>
2019-04-12 22:21:53 +1200

    refactored some stuff in business controller

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -10,27 +10,15 @@ class BusinessesController < ApplicationController
   end
 
   def spotify_user
-    # binding.pry
     spotify_user = RSpotify::User.new(auth) #stores user's spotify data as spotify_user
+
     @business = Business.find_or_create_by(email_address: spotify_user.email) do |b| #finds or creates new business object using spotify id
-      # binding.pry
-      b.uid = spotify_user.id
+      b.uid = spotify_user.id #sets uid as spotify user id so later i can extent this app to allow for playback
       b.password = SecureRandom.urlsafe_base64 #generates a random password for spotify strategy
-      b.name = spotify_user.display_name
+      b.name = spotify_user.display_name #sets name
     end
 
-    spotify_user.playlists.each do |playlist| #begins iteration over each of spotify_user's playlist objects and
-      s_playlist = Playlist.find_or_create_by(name: playlist.name.to_s) #creates a new Playlist for each object
-      s_playlist.business = @business #associates the Playlist with the business
-      s_playlist.save #saves the Playlist object
-        playlist.tracks.each do |track| #iterates over each track in the playlist
-          song = Song.find_or_create_by(name: track.name, artist: track.artists.first.name, track_id: track.id) #creates a new Song object for each track in the playlist
-          song.playlist = s_playlist #saves the song in the newly created playlist object
-          s_playlist.songs << song #saves the song in the newly created playlist object
-          s_playlist.save #saves the playlist
-        end
-      end
-      # binding.pry
+    build_playlists(@business)
     @business.save
     session[:business_id] = @business.id
     redirect_to business_path(@business)
@@ -70,7 +58,18 @@ class BusinessesController < ApplicationController
     request.env['omniauth.auth']
   end
 
-  def build_playlists
-  end 
+  def build_playlists(business)
+    RSpotify::User.new(auth).playlists.each do |playlist| #begins iteration over each of spotify_user's playlist objects and
+      s_playlist = Playlist.find_or_create_by(name: playlist.name.to_s) #creates a new Playlist for each object
+      s_playlist.business = business #associates the Playlist with the business
+      s_playlist.save #saves the Playlist object
+        playlist.tracks.each do |track| #iterates over each track in the playlist
+          song = Song.find_or_create_by(name: track.name, artist: track.artists.first.name, track_id: track.id) #creates a new Song object for each track in the playlist
+          song.playlist = s_playlist #saves the song in the newly created playlist object
+          s_playlist.songs << song #saves the song in the newly created playlist object
+          s_playlist.save #saves the playlist
+        end
+      end
+  end
 
 end
