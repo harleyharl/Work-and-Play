6f6edb481fc84b62282310305465192f28127a91
harleyharl <harleymteden@gmail.com>
2019-05-07 10:11:15 -0700

    added :set_business before action to DRY up code, and set show action as the exception to allow for different urls accessing the same resource (location/playlist and business/playlist)

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,21 +1,29 @@
 class PlaylistsController < ApplicationController
-  before_action :require_login
+  before_action :require_login, :set_business, except: [:show]
 
   def index
-    @business = Business.find_by(id: session[:business_id])
     @location = Location.find_by(id: params[:location_id])
     @playlists = @business.playlists.all
   end
 
 
   def show
+    @business = Business.find_by(id: session[:business_id])
     if params[:location_id] #deals with different URLS - location_playlist or business_playlist
-      @business = Business.find_by(id: session[:business_id])
       @location = Location.find_by(id: params[:location_id])
       @playlist = Playlist.find_by(id: params[:id])
     else
-      @business = Business.find_by(id: session[:business_id])
       @playlist = Playlist.find_by(id: params[:id])
+    end
+  end
+
+  private
+
+  def set_business
+    if params[:business_id].to_i == session[:business_id]
+      @business = Business.find_by(id: params[:business_id])
+    else
+      redirect_to badurl_path
     end
   end
 
