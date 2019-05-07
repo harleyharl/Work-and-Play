0a4a3c0100678d6e495578ad54dc11c843c68caa
harleyharl <harleymteden@gmail.com>
2019-05-07 10:13:23 -0700

    changed renders to redirects so page would reload so flash messages no longer persist. edited create path to fix issue where no error would display for people entering non-existent emails. added badurl action to deal with users entering bad urls

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,23 +12,33 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # binding.pry
     if params[:email_address].blank? || params[:password].blank?
       flash[:notice] = "Looks like you've forgotten to enter your email address or password. Please try again or sign up for an account"
-      render :new
+      redirect_to login_path
     elsif @business = Business.find_by(email_address: params[:email_address])
       if @business.authenticate(params[:password])
         session[:business_id] = @business.id
         redirect_to business_locations_path(@business)
       else
         flash[:notice] = "You have entered the wrong email or password. Please try again or sign up for an account"
-        render :new
+        redirect_to login_path
       end
+    else
+      flash[:notice] = "You don't seem to have an account. Please try again or sign up for an account"
+      redirect_to login_path
     end
   end
 
   def destroy
     reset_session
     redirect_to root_path
+  end
+
+  def badurl
+    # binding.pry
+    @business = session[:business_id]
+    render :bad_url
   end
 
   private
