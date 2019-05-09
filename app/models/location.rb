a2871188d7ad4e4ca5b3b32b779829ecc6321fb3
harleyharl <harleymteden@gmail.com>
2019-05-09 07:14:18 -0700

    added notes

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,10 +5,10 @@ class Location < ApplicationRecord
   belongs_to :business
   has_many :location_playlists
   has_many :playlists, through: :location_playlists
-  accepts_nested_attributes_for :playlists
+  accepts_nested_attributes_for :playlists #redundant as im not using fields_for
 
-  def self.by_state(state)
+  def self.by_state(state) #use scope method!
     where(state: state)
   end
-  
+
 end
