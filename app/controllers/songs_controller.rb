4f230e9a71f2696dd2ac25f543ee2ae5093484b0
harleyharl <harleymteden@gmail.com>
2019-05-09 11:37:25 -0700

    added more_songs_by_artist method which uses scope method from Song class to lookup more artists via a passed in argument (I will need to add Artist model to really get it working properly)

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,16 @@
 class SongsController < ApplicationController
   before_action :require_login
+  before_action :set_business
 
   def destroy
     @playlist = Playlist.find_by(id: params[:playlist_id])
     Song.find_by(id: params[:song_id]).destroy
     @playlist.save
     redirect_to playlist_path(@playlist)
+  end
+
+  def more_songs_by_artist
+    @songs = Song.more_songs_by_artist("Mt. Eden")
   end
 
 end
