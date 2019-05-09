c90e5230765b08cf51a719664d731c9c5ecd42b1
harleyharl <harleymteden@gmail.com>
2019-05-09 11:38:13 -0700

    added some scope methods for fun and started building set_energy_levels to take that method out of the controller (it shouldn't be there)

--- app/models/location_playlist.rb
+++ app/models/location_playlist.rb
@@ -1,4 +1,19 @@
 class LocationPlaylist < ApplicationRecord
   belongs_to :location
   belongs_to :playlist
+  scope :high_energy, -> { where(energy: 'High')}
+  scope :low_energy, -> { where(energy: 'Low')}
+  scope :at_location_2, -> { where(location_id: 2)}
+
+  # def set_energy_levels(location, params)
+  #   count = 0
+  #   all.each do |locationplaylist|
+  #     if locationplaylist.location_id == location.id
+  #       locationplaylist.energy = eval("params[:energy_level#{count}]")
+  #       locationplaylist.save
+  #       count += 1
+  #     end
+  #   end
+  # end
+
 end
