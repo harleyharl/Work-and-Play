6bfc8c16e89718b5739b626db0f8f5baaf4946da
harleyharl <harleymteden@gmail.com>
2019-05-31 07:51:39 -0700

    removed colorful title

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,1 @@
-<script type="text/javascript">
-  $(document).ready(function(){
-    var randomColor = Math.floor(Math.random() * 255);
-    console.log(randomColor);
-    $('h1').on('click', function(){
-      $(this).css('color','rgb('randomColor' + , + 'randomColor' + , + 'randomColor')');
-    })
-  })
-</script>
+
