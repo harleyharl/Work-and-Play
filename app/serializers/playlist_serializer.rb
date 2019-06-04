98dc281c60ecbac6d8356d1117ac1772f5873618
harleyharl <harleymteden@gmail.com>
2019-06-04 10:58:52 -0700

    removed unused code

--- app/serializers/playlist_serializer.rb
+++ app/serializers/playlist_serializer.rb
@@ -1,8 +1,4 @@
 class PlaylistSerializer < ActiveModel::Serializer
   attributes :id, :name
   has_many :songs
-
-  # belongs_to :business
-  # has_many :location_playlists
-  # has_many :locations, through: :location_playlists
 end
