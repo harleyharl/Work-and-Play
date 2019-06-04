5a96e5b903b9023f17e700f0ae45ce7e539c81fe
harleyharl <harleymteden@gmail.com>
2019-06-03 17:29:36 -0700

    refactored all classes to es6 syntax. refactored event listeners for cleanliness. added edit locations link for after new location form is submitted to allow users to edit locations straight away

--- app/assets/javascripts/playlists.js
+++ app/assets/javascripts/playlists.js
@@ -1,24 +1,115 @@
-var businessDetails; //probably need to change this
+let businessDetails; //probably need to change this
 const timeSlots = ["6am-9am", "9am-12pm", "12pm-3pm", "3pm-6pm", "6pm-9pm"]
 let timeSlotIndex = 0
-
 
 fetch(`${window.origin}/show_json.json`) // gets the user's details and saves the into businessDetails variable
   .then(res => res.json())
   .then(data => businessDetails = data)
 
+// binds all click handlers
+
 $(() => {
   bindClickHandlers()
 })
 
+const bindClickHandlers = () => { // attaches event listeners when executed
 
-const bindClickHandlers = () => { //repaints the dom with my music playlists (just the titles)
-  $(`#my-music`).on('click', e => {
+  paintPlaylists(`#my-music`)
+  paintPlaylists(`#my-music2`)
+  paintPlaylistSongs()
+  paintLocation()
+  submitAndPaintNewLocation()
+
+}
+
+class Playlist {
+  constructor(playlist){
+    this.id = playlist.id
+    this.name = playlist.name
+    this.songs = playlist.songs
+  }
+
+  formatPlaylist() {
+    let playlistHtml = `
+      <h3> ${this.name} </h3>
+      <p> ${timeSlots[timeSlotIndex]} </p>
+    `
+    return playlistHtml
+  }
+
+  formatPlaylistForMusicPage() {
+    let playlistHtmlForMusicPage = `
+      <h3>${this.name}</h3>
+      <a class="playlist" id="playlist-${this.id}" href="#"> click for details </a>
+      <div class="newclass" id="playlistlist-${this.id}"></div>
+    `
+    return playlistHtmlForMusicPage
+  }
+}
+
+class Song {
+  constructor(song){
+    this.id = song.id
+    this.name = song.name
+    this.artist = song.artist
+    this.track_id = song.track_id
+  }
+
+  formatIndex() {
+    let songHtml = `
+      <li> ${this.artist} - ${this.name} </li>
+    `
+    return songHtml
+  }
+}
+
+class Location {
+  constructor(location) {
+    this.id = location.id
+    this.name = location.name
+    this.address = location.address
+    this.state = location.state
+  }
+
+  formatLocation() {
+    let locationHtml = `
+      <h2>${this.name}</h2>
+      <p>${this.address}, ${this.state}</p>
+    `
+    return locationHtml
+  }
+}
+
+class Business {
+  constructor(businessDetails) {
+    this.id = businessDetails.id
+    this.name = businessDetails.name
+  }
+
+  urlSlug() { //gets the businesses' base url *im never nesting my resources again....lesson learned
+    let name = businessDetails.name.toLowerCase().replace(/[^a-z0-9]+/g,'-').replace(/(^-|-$)/g,'');
+    let id = this.id
+    return(id + '-' + name)
+  }
+}
+
+// other random bits and pieces
+
+if (window.location.hash && window.location.hash == '#_=_') { // handles the weird spotify / facebook redirect thing in my url bar
+        window.location.hash = '';
+    }
+
+let backToYourLocations = `
+  <br>
+  <br>
+  <a id="back" href='${window.location.pathname}'>See your other locations</a><br>
+`
+
+paintPlaylists = function(idTag){
+  $(idTag).on('click', e => {
     let business = new Business(businessDetails)
     let url = e.currentTarget.href
-
     e.preventDefault()
-
     history.pushState(null, null, url) // put the current in the first null position. this might cause problems if theres double digits of locations..use split instead
     fetch(`${e.target.href}.json`)
       .then(res => res.json())
@@ -33,7 +124,9 @@ const bindClickHandlers = () => { //repaints the dom with my music playlists (ju
         })
       })
   })
+}
 
+paintPlaylistSongs = function(){
   $(`#app-container`).on('click', '.playlist', function(e) { //event delegation so these events can be fired from our re-painted dom
     e.preventDefault()
     let business = new Business(businessDetails) //creates a new variable for the business
@@ -48,7 +141,9 @@ const bindClickHandlers = () => { //repaints the dom with my music playlists (ju
         })
       })
   })
+}
 
+paintLocation = function(){
   $(`.location`).on('click', e => { // repaints the dom with the locations index of playlists (this is the 'show requirement')
     e.preventDefault()
     let currentUrl = e.currentTarget.baseURI
@@ -70,7 +165,9 @@ const bindClickHandlers = () => { //repaints the dom with my music playlists (ju
       $(`#app-container`).append(`<a href=${window.location.href}/edit>Edit this location</a>`)
     })
   })
+}
 
+submitAndPaintNewLocation = function() {
   $(`#new_location`).on('submit', function(e) { //satisfies the form requirement
     e.preventDefault()
     serializedLocationValues = ($(this).serialize())// grabs values from form submit. 'this' is the form itself
@@ -80,87 +177,17 @@ const bindClickHandlers = () => { //repaints the dom with my music playlists (ju
       const newLocation = new Location(data)
       const newLocationHtml = newLocation.formatLocation()
       otherLocationsLink = `<a href=`+`${window.location.origin}`+`/businesses/`+`${business.urlSlug()}`+`/locations>See your other locations</a>`
-      // editLocationLink =  `<a href=`+`${window.location.origin}`+`/businesses/`+`${business.urlSlug()}`+`/locations/`+`${window.location.href.slice(-1)}`+`/edit`+`>Edit this location</a>`
+      history.pushState(null, null, `${data.id}`)
+      editLocationLink =  `<a href=`+`${window.location.origin}`+`/businesses/`+`${business.urlSlug()}`+`/locations/`+`${data.id}`+`/edit`+`>Schedule playlists for this location</a>`
       $('#app-container').html(newLocationHtml)
       $('#app-container').append(otherLocationsLink)
-      // $('#app-container').append(editLocationLink)
+      $('#app-container').append("<br>")
+      $('#app-container').append(editLocationLink)
     })
   })
-
-  $(window).bind('popstate', function(){
-    window.location.href = window.location.href;
-  });
-
 }
 
-function Playlist(playlist) {
-  this.id = playlist.id
-  this.name = playlist.name
-  this.songs = playlist.songs
-}
-
-Playlist.prototype.formatPlaylistForMusicPage = function(){
-  let playlistHtmlForMusicPage = `
-
-    <h3>${this.name}</h3>
-
-    <a class="playlist" id="playlist-${this.id}" href="#"> click for details </a>
-    <div class="newclass" id="playlistlist-${this.id}"></div>
-  `
-  return playlistHtmlForMusicPage
-}
-
-Playlist.prototype.formatPlaylist = function() {
-  let playlistHtml = `
-    <h3> ${this.name} </h3>
-    <p> ${timeSlots[timeSlotIndex]} </p>
-  `
-  return playlistHtml
-}
-
-// below needs to be fixed because it doesn't work after creating a new location
-let backToYourLocations = `
-  <br>
-  <br>
-  <a id="back" href='${window.location.pathname}'>See your other locations</a><br>
-`
-
-function Song(song) {
-  this.id = song.id
-  this.name = song.name
-  this.artist = song.artist
-  this.track_id = song.track_id
-}
-
-Song.prototype.formatIndex = function() {
-  let songHtml = `
-    <li> ${this.artist} - ${this.name} </li>
-  `
-  return songHtml
-}
-
-function Location(location) {
-  this.id = location.id
-  this.name = location.name
-  this.address = location.address
-  this.state = location.state
-}
-
-Location.prototype.formatLocation = function(){
-  let locationHtml = `
-    <h2>${this.name}</h2>
-    <p>${this.address}, ${this.state}</p>
-  `
-  return locationHtml
-}
-
-function Business(businessDetails){ //business class constructor
-  this.id = businessDetails.id
-  this.name = businessDetails.name
-}
-
-Business.prototype.urlSlug = function(){ //gets the businesses' base url *im never nesting my resources again....lesson learned
-  let name = businessDetails.name.toLowerCase().replace(/[^a-z0-9]+/g,'-').replace(/(^-|-$)/g,'');
-  let id = this.id
-  return(id + '-' + name)
-}
+// makes back button work with rendered javascript pages
+$(window).bind('popstate', function(){
+  window.location.href = window.location.href;
+});
